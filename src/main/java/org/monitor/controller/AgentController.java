6236bfe82b104949ea88d81f04f733d695dbfdcc
cpzou <zouchangpeng@payworth.com>
2022-11-12 22:30:54 +0800

    ♿️ use properties

--- src/main/java/org/monitor/controller/AgentController.java
+++ src/main/java/org/monitor/controller/AgentController.java
@@ -7,6 +7,7 @@ import com.sun.tools.attach.VirtualMachine;
 import lombok.extern.slf4j.Slf4j;
 import org.monitor.entity.AgentAction;
 import org.monitor.entity.Result;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,10 +20,11 @@ import static org.monitor.service.JvmService.connectPid;
 @RequestMapping("agent")
 public class AgentController {
 
+    @Value("${agent.jar.path}")
+    private String agentJarPath;
 
     @PostMapping
     public Result agent(@RequestBody @Validated AgentAction action) throws IOException, AttachNotSupportedException, AgentLoadException, AgentInitializationException, InterruptedException {
-        String agentJarPath = "/Users/cpzou/IdeaProjects/mango-monitor/agent/target/agent-1.0-SNAPSHOT.jar";
         VirtualMachine virtualMachine = VirtualMachine.attach(String.valueOf(connectPid));
 
         log.info("connectPid is {}", connectPid);
